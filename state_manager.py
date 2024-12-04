c7c2b602ff82bb9b5a5b11039de306e3b46e4a87
datjandra <donny.tjandra@gmail.com>
2024-12-04 15:36:22 -0800

    Delete state_manager.py

--- state_manager.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-def initialize_session_state():
-    # Initialize session state variables if they don't exist
-    st.session_state.setdefault("username", None)
-    st.session_state.setdefault("role", None)
-    st.session_state.setdefault("is_logged_in", False)
