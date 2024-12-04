a01c51006bff6fe0a72a407c7b26eb02e83dd282
datjandra <donny.tjandra@gmail.com>
2024-12-04 11:07:30 -0800

    Update login.py

--- login.py
+++ login.py
@@ -16,6 +16,11 @@ def login_page(col):
       password = st.text_input("Password", type="password")
       if st.button("Login"):
         user = get_user(username)
+
+        st.write(user)
+        st.write(user.shape)
+        st.write(user["password"])
+          
         if user and bcrypt.checkpw(password.encode(), user["password"].item()):
             st.session_state["role"] = user[2]
             st.success(f"Logged in as {user[2]}")
