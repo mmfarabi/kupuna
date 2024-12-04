0f43c06265f713c56adbc8893d2fd3bd4e7e1cf8
datjandra <donny.tjandra@gmail.com>
2024-12-04 14:40:50 -0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,11 @@
 import streamlit as st
 
 def check_access(role):
-  if role and st.session_state.get("role") != role:
+  if st.session_state.get("role") != role:
     st.error("You do not have permission to view this page.")
     st.switch_page("login.py")  # Redirect to login page
+
+def check_login():
+  if not st.session_state.get("role"):
+    st.error("You must be logged in to view this page.")
+    st.switch_page("login.py")  # Redirect to the login page
