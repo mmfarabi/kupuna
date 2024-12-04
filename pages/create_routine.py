41715640f767ec4a2e09001425fbb7b73c42426f
datjandra <donny.tjandra@gmail.com>
2024-12-04 15:38:11 -0800

    Update create_routine.py

--- pages/create_routine.py
+++ pages/create_routine.py
@@ -1,11 +1,6 @@
 import streamlit as st
 
-from utils import check_access
-
 def main():
-    # Check access
-    check_access("coach")
-
     st.title("Create Exercise Routine")
     st.write("This is a placeholder for the Create Exercise Routine page.")
 
