6f4110c4cb6747dcb21beff71f83b4660f5a9c2d
datjandra <donny.tjandra@gmail.com>
2024-12-05 12:16:03 -0800

    Update create_routine.py

--- pages/create_routine.py
+++ pages/create_routine.py
@@ -1,8 +1,24 @@
 import streamlit as st
 
+from style_helper import apply_header
+
 def main():
+    apply_header()
     st.title("Create Exercise Routine")
-    st.write("This is a placeholder for the Create Exercise Routine page.")
+
+    st.markdown(
+      """
+      <div class="button-grid">
+          <a href="assign_routine" class="button-card">
+            <p>Assign Routine</p>
+            <div class="icon">&#128116;</div>
+          </a>
+          <a href="exercise_routines" class="button-card">
+            <p>View Routines</p>
+            <div class="icon">&#128221;</div>
+          </a>
+      </div>
+      """, unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
