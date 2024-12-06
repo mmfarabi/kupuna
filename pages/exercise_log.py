2ae7867aa3deb6bd688e3dd9a9dd53f9b1cf987c
datjandra <donny.tjandra@gmail.com>
2024-12-05 19:20:30 -0800

    Update exercise_log.py

--- pages/exercise_log.py
+++ pages/exercise_log.py
@@ -1,8 +1,23 @@
 import streamlit as st
 
+from style_helper import apply_header
+
 def main():
+    apply_header()
+
     st.title("Exercise Log")
-    st.write("This is a placeholder for the Exercise Log page.")
+
+    st.markdown(
+      """
+      <div class="button-grid">
+          <a href="exercise_routines" target="_self" class="button-card">
+            <p>View Routines</p>
+            <div class="icon">&#128221;</div>
+          </a>
+      </div>
+      """, unsafe_allow_html=True)
+
+    st.sidebar.image("https://raw.githubusercontent.com/datjandra/kupuna/refs/heads/main/images/logo.png")
 
 if __name__ == "__main__":
     main()
