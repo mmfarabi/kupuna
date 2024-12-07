6b3992fd3a3bd2b6324e650e7059ea6d08458b10
datjandra <donny.tjandra@gmail.com>
2024-12-07 09:35:56 -0800

    Update member_info.py

--- pages/member_info.py
+++ pages/member_info.py
@@ -139,8 +139,8 @@ def main():
         # Select only the required columns
         final_data = merged_data[["PRIMARY_PERSON_KEY", "NAME", "MEM_AGE", "MEM_GENDER", "MEM_RACE", "MEM_ETHNICITY"]]
     
-        bulk_insert_patient(final_data)
-        st.rerun()
+        patient_df = bulk_insert_patient(final_data)
+        st.dataframe(patient_df)
 
 if __name__ == "__main__":
     main()
