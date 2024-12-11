170224ef1d39cc30e3f3a0a589f21968224bb882
datjandra <donny.tjandra@gmail.com>
2024-12-10 15:21:47 -1000

    Update assign_routine.py

--- pages/assign_routine.py
+++ pages/assign_routine.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import streamlit_shadcn_ui as ui
 
-from style_helper import apply_header, card_container
+from style_helper import apply_header
 from database import fetch_patients, fetch_routines, assign_patient_to_routine
 
 def main():    
